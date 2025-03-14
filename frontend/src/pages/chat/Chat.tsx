e721e0ce3546b67dbd18b96146728082658788b8
Heather <heathriel@gmail.com>
2025-03-13 17:50:54 -0700

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -14,6 +14,7 @@ import { nord } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import styles from './Chat.module.css'
 import Contoso from '../../assets/Contoso.svg'
 import { XSSAllowTags } from '../../constants/sanatizeAllowables'
+import MiddleSection from './MiddleSection';
 
 import {
   ChatMessage,
@@ -763,7 +764,7 @@ const Chat = () => {
       makeApiRequestWithoutCosmosDB(question);
     }
   }
-  
+
   return (
     <div className={styles.container} role="main">
       {showAuthMessage ? (
